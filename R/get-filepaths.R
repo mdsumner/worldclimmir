05eb7bb1ef4ffa4cf8635ff1d0cc19e59cd199ec
Michael Sumner <mdsumner@gmail.com>
2025-03-27 05:38:07 +1100

    setup

--- R/get-filepaths.R
+++ R/get-filepaths.R
@@ -1,6 +1,5 @@
-library(bowerbird)
 
-
+get_filepaths <- function() {
 ## we aren't downloading
 my_directory <- tempdir()
 cf <- bb_config(local_file_root = my_directory)
@@ -15,8 +14,7 @@ src <-
     citation = " [access date]",
     source_url = "https://geodata.ucdavis.edu/climate/worldclim",
     license = "Please cite",
-    relative = TRUE,
-    method = list("bb_handler_rget", level = Inf),
+    method = list("bb_handler_rget", level = Inf, relative = TRUE),
     postprocess = NULL,
     access_function = c("terra::rast", "terra::vect")[1],
     collection_size = 1e10,
@@ -33,12 +31,15 @@ allurls <- do.call(rbind, status$files)
 #size <- numeric(length(allurls))
 
 options(parallelly.fork.enable = TRUE, future.rng.onMisuse = "ignore")
-library(furrr); plan(multicore)
+future::plan(multicore)
 
 allurls$sizes <- future_map_dbl(allurls$url, \(.url) as.numeric(gdalraster::vsi_stat(sprintf("/vsicurl/%s", .url), "size")))
 plan(sequential)
-sum(sizes)/1e9
+#sum(sizes)/1e9
 #[1] 5479.997
+allurls
+}
 
-nanoparquet::write_parquet(allurls, "geodata.parquet")
-
+writefile <- function(x) {
+  nanoparquet::write_parquet(x, "geodata.parquet")
+}
